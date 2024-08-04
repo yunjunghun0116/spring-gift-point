33340c91db13a74ee60a813c2dfac89e83cae40f
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-08-04 19:36:47 +0900

    add: 모든 요청에 대해서 허용하도록 함

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "localhost:3000", "https://kauth.kakao.com", "https://kapi.kakao.com")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
